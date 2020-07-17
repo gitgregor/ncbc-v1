5973768aa281ebb0c622f8ce96dba8055ac2d442
Gregory <gerda.trust@gmail.com>
2020-07-17 13:32:03 +0200

    delete lock files

--- app.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest')
-const { app } = require('./express/app');
-
-it('works', async () => {
-    const response = await request(app).get('/');
-    expect(response).toMatchSnapshot()
-    console.log(response)
-})
